c6e35de7db56c2f9241ac200ffa1c385aeaf3462
AndresJairSotoADSI <ynfinityink@gmail.com>
2022-10-14 14:54:41 -0500

    Ejercicio8-Guia3 Javascript

--- main.js
+++ main.js
@@ -1,15 +1,23 @@
-let tdt=Number(prompt('ingrese los vendedores contratados '));
-let sbt=Number(prompt('ingrese el sueldo vase del trabajador '));
-let cont=1;
+let con=1
+let cref=Number(prompt("Digite los creditos de la materia de fundamentos:"));
+let creb=Number(prompt("Digite los creditos de la materia bd:"));
+let cree=Number(prompt("Digite los creditos de la materia de etica:"));
+let can=Number(prompt("Digite la cantidad de estudiantes:"));
+while (con<=can){
+    let calf=Number(prompt("Digite la calificacion de la materia de funadamentos:"));
+    let calb=Number(prompt("Digite la calificacion de la materia de bd:"));
+    let cale=Number(prompt("Digite la calificacion de la materia de etica"));
 
-while(cont<=tdt){
-    let venta1=Number(prompt('digite la primera venta: '));
-    let venta2=Number(prompt('digite la segunda venta: '));
-    let venta3=Number(prompt('digite la tercera venta: '));
-    let comi=(venta1 + venta2 + venta3)*0.10;
-    let sult=comi+sbt;
-    alert('las comiciones del trabajador por las tres ventas es '+comi),
-    alert('el sueldo total del trabajador por semana es '+sult);
-    cont+=1;
-}
+    let mat1=calf*cref;
+    let mat2=calb*creb;
+    let mat3=cale*cree;
+    /*suma de creditos lo siguiente*/
+    let cre=cref+creb+cree;
+
+    /*#sumamos todas las materias divido en el total de sus creditos*/
+    let pon=(mat1+mat2+mat3)/cre;
+    alert("su promedio ponderado es de:"+pon);
+    con+=1 
+} 
+
 alert("Fin del proceso");  
