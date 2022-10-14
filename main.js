1854b505f73385290d409d0b85821f9d511a1fc3
AndresJairSotoADSI <ynfinityink@gmail.com>
2022-10-14 15:52:13 -0500

    Ejercico11-Guia3 Javascript

--- main.js
+++ main.js
@@ -1,25 +1,25 @@
-let vehiculos = Number(prompt("Ingrese La cantidad de vehivulos"))
-let cont=1;
-let autobus=0;
-let camion=0;
-let motocicleta=0;
-let turismo=0;
-let pasajeros=0;
-while(vehiculos>=cont){
-    let vehiculo= prompt("Ingrese el tipo de vehiculo");
-    if (vehiculo=="autobus"){
-        autobus++;
-    } else if (vehiculo=="camion"){
-        camion++;
-    } else if (vehiculo=='motocicleta'){
-        motocicleta++;
-    } else if (vehiculo='turismo'){
-        pasajeros+=Number(prompt("Cantidad de turistas en el vehiculo: "));
-        turismo++;
+cantidad=Number(parse("Cantidad de autos que entraron ala ciudad "));
+while ((con <=cantidad)) {
+    placa = Number(prompt("Digite el ultimo digito de placa"));
+    if (((placa === 1) || (placa === 2))) {
+        alert("Su color es amarilla");
+    } else {
+        if (((placa === 3) || (placa === 4))) {
+            alert("Su color es rosado");
+        } else {
+            if (((placa === 5) || (placa === 6))) {
+                alert("Su color es rojo");
+            } else {
+                if (((placa === 7) || (placa === 8))) {
+                    alert("Su color es verde");
+                } else {
+                    if (((placa === 9) || (placa === 0))) {
+                        alert("Su color es azul");
+                    } else {
+                        alert("Vuelva a digitar los codigos:");
+                    }
+                }
+            }
+        }
     }
-    cont++;
 }
-alert("La cantidad de autobuses son"+autobus);
-alert("La cantidad de camion son:"+camion);
-alert("La cantidad de motocicleta son:"+motocicleta);
-alert("La cantidad de turistas son: "+turismo+ "y sus pasajeros totales son "+pasajeros);
