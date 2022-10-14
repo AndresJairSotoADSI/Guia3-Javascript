4d67a7d411511527cfb000be98308573bd19644b
AndresJairSotoADSI <ynfinityink@gmail.com>
2022-10-14 14:47:00 -0500

    Ejercicio4-Guia3 Javascript

--- main.js
+++ main.js
@@ -1,11 +1,8 @@
-let cantidadepersonas=Number(prompt("ingresar la cantidad de personas:"));
-while (cont<=cantidadepersonas){
-    let nombre=String(prompt("escriba el nombre:"));
-    let apellido=String(prompt("escriba el apellido:"));
-    let edad=Number(prompt("numero de edad:"));
-    let notapro=Number(prompt("ingresar le nota promedio:"));
-    let datospersonales=("nombre"+nombre+"apellido"+apellido+"edad"+edad+"notanotapro"+notapro);
-    alert("los datos por personas son:"+datospersonales);
-    cont+=1;
+let caballeros = 8;
+let damas =0;
+while (caballeros + damas<=42){
+    damas+=1
+    caballeros+=1 
+    alert('damas que habia en el baile son'+damas)
 }
 alert("Fin del proceso") ;
