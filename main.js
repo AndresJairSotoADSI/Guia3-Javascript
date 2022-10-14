4d495cdb93828d361ff250c1801caf23b37791d4
AndresJairSotoADSI <ynfinityink@gmail.com>
2022-10-14 14:53:03 -0500

    Ejercicio7-Guia3 Javascript

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
-let segun=0;
-let avion=222222;
-let misil=222222;
-let aceleracionavion=20;
-let aceleracionmilsil=10; 
-while(aceleracionavion-aceleracionmilsil<=1000){
-    aceleracionavion+=20+aceleracionavion;
-    aceleracionmilsil+=10+aceleracionmilsil;
-    segun+=1;
-    alert("segundo"+segun);
-    alert("Distancia del avion"+aceleracionavion);
-    alert("Distancia del misil"+aceleracionmilsil);
+let tdt=Number(prompt('ingrese los vendedores contratados '));
+let sbt=Number(prompt('ingrese el sueldo vase del trabajador '));
+let cont=1;
 
+while(cont<=tdt){
+    let venta1=Number(prompt('digite la primera venta: '));
+    let venta2=Number(prompt('digite la segunda venta: '));
+    let venta3=Number(prompt('digite la tercera venta: '));
+    let comi=(venta1 + venta2 + venta3)*0.10;
+    let sult=comi+sbt;
+    alert('las comiciones del trabajador por las tres ventas es '+comi),
+    alert('el sueldo total del trabajador por semana es '+sult);
+    cont+=1;
 }
-alert("Fin del proceso") ;
+alert("Fin del proceso");  
